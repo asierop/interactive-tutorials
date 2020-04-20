785f42a11f5e6bb552dc250dc96080cb8ece4f6f
asierop <63303513+asierop@users.noreply.github.com>
2020-04-20 23:11:08 +0200

    Create Learnpyton_VariablesandTypes_solution.py
    solucion al ejercicio del módulo 2 de learnpython.

---
+++ Learnpyton_VariablesandTypes_solution.py
@@ -0,0 +1,12 @@
+# Escribe tu propio codigo aqui
+micadena = "hola"
+miflotante = 10.0
+miIntegral = 20
+
+# probando el codigo
+if micadena == "hola":
+    print ("Cadena:",micadena)
+if isinstance(miflotante, float) and miflotante == 10.0:
+    print ("Flotante:",miflotante)
+if isinstance(miIntegral, int) and miIntegral == 20:
+    print ("Integral:",miIntegral)
